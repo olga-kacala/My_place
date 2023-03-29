937d2103353ea9b4407c514b0bb828bb18f18bdd
olga-kacala <olga.kacala@gmail.com>
2023-03-29 19:33:30 +0200

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,84 +1,22 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { SingleCard } from "./components/SingleCard";
 
-const cardImages = [
-  { src: "/img/Pola.png", matched: false },
-  { src: "/img/cactus.png", matched: false },
-  { src: "/img/banana.png", matched: false },
-  { src: "/img/noisyPola.png", matched: false },
-  { src: "/img/cat.png", matched: false },
-  { src: "/img/catInGlasses.png", matched: false },
-];
+import "./App.css";
+
+import { Routes, Route} from "react-router-dom";
+
+
 
 function App() {
-  const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
-  const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null);
-  const [disabled, setDisabled] = useState(false);
-
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages]
-      .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }));
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setCards(shuffledCards);
-    setTurns(0);
-  };
-
-  const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-  };
-
-  useEffect(() => {
-    if (choiceOne && choiceTwo) {
-      setDisabled(true);
-      if (choiceOne.src === choiceTwo.src) {
-        setCards((prevCards) => {
-          return prevCards.map((card) => {
-            if (card.src === choiceOne.src) {
-              return { ...card, matched: true };
-            } else {
-              return card;
-            }
-          });
-        });
-        resetTurn();
-      } else {
-        setTimeout(() => resetTurn(), 1000);
-      }
-    }
-  }, [choiceOne, choiceTwo]);
-
-  const resetTurn = () => {
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setTurns((prevTurns) => prevTurns + 1);
-    setDisabled(false);
-  };
-
-  useEffect(() => {
-    shuffleCards();
-  }, []);
+  
 
   return (
     <div className="App">
-      <h1>Memory Match</h1>
-      <button onClick={shuffleCards}>New Game</button>
-      <div className="cardGrid">
-        {cards.map((card) => (
-          <SingleCard
-            key={card.id}
-            card={card}
-            handleChoice={handleChoice}
-            flipped={card === choiceOne || card === choiceTwo || card.matched}
-            disabled={disabled}
-          />
-        ))}
-      </div>
-      <p>Turns:{turns}</p>
+<Routes>
+<Route path="/memory" element={<Memory/>}/>
+<Route path="/snake" element={<Snake/>}/>
+</Routes>
+<Footer/>
+      
+
     </div>
   );
 }
