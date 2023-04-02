8bbdfc9a480bad12312230df069aa8dc9cee5767
olga-kacala <olga.kacala@gmail.com>
2023-04-02 17:17:23 +0200

    home page yupdate

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,32 @@
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 import { useNavigate } from "react-router-dom";
+import classes from "./Home.module.css";
+
 
 export const Home = (): ReactJSXElement => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+
+
+
+  return (
+    <div className={classes ["Home"]}>
+        <div><img
+        alt="Card back"
+        src={"/img/cover.png"}
+        onClick={() => navigate("/memory")}
+      /></div>
+      <div>  
+        <img
+        alt="Snake"
+        src={"/img/snake.png"}
+        onClick={() => navigate("/snake")}
+      /></div>
+      
+    <a href="/img/OLGA_KACALA_CV_ENG.pdf" download="O_Kacala_CV.pdf">
+  Download CV
+</a>
+
     
-    return (
-<div>
-   
-<img
-          alt="Card back"
-          src={"/img/cover.png"}
-          onClick={()=> navigate('/memory')}
-        />  
-        
-</div>
-    )
-}
+    </div>
+  );
+};
