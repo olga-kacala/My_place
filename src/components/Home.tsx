5cbb84eecf0cf7517e79c4070c86f090e8a128fa
olga-kacala <olga.kacala@gmail.com>
2023-04-02 14:56:30 +0200

    home page yupdate

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,18 @@
-import { ReactJSXElement } from "@emotion/react/types/jsx-namespace"
+import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
+import { useNavigate } from "react-router-dom";
 
 export const Home = (): ReactJSXElement => {
+    const navigate = useNavigate();
+    
     return (
 <div>
-    
+   
+<img
+          alt="Card back"
+          src={"/img/cover.png"}
+          onClick={()=> navigate('/memory')}
+        />  
+        
 </div>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,6 @@ export const Navbar = (): JSX.Element => {
   return (
     <div className={classes ["Nav"]}>
       <Link to='/'>Home</Link>
-      <div>
-      <Link to='memory'>Memory</Link>
-        </div>
-    <div><Link to='snake'>Snake</Link></div>
     </div>
   );
 };
