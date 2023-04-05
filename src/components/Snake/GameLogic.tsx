81320c210a6add645711a4eee417f40d10559c67
olga-kacala <olga.kacala@gmail.com>
2023-04-05 19:11:01 +0200

    Setting the movement directions

--- src/components/Snake/GameLogic.tsx
+++ src/components/Snake/GameLogic.tsx
@@ -1,20 +1,55 @@
 import { useState } from "react";
+import { UseInterval } from "../Utils/UseInterval";
 
 export interface Position {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 }
+
+export enum Direction {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
+const movementSpeed = 100;
 
 export const GameLogic = () => {
+  const [direction, setDirection] = useState<Direction>();
+  const [snakeBody, setSnakeBody] = useState<Position[]>([
+    {
+      x: 0,
+      y: 0,
+    },
+  ]);
 
-    const [snakeBody, setSnakeBody] = useState<Position[]> ([
-        {
-            x: 0,
-            y: 0,
-        }
-    ]);
-    return {
-        snakeBody,
+  const onKeyDownHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    switch (event.code) {
+      case "KeyS":
+        setDirection(Direction.DOWN);
+        break;
+
+      case "KeyW":
+        setDirection(Direction.UP);
+        break;
+
+      case "KeyD":
+        setDirection(Direction.RIGHT);
+        break;
+
+      case "KeyA":
+        setDirection(Direction.LEFT);
+        break;
     }
+    console.log(event.code);
+  };
 
-}
+  const moveSnake = () => {};
+
+  UseInterval(moveSnake, movementSpeed);
+
+  return {
+    snakeBody,
+    onKeyDownHandler,
+  };
+};
--- src/components/Snake/Snake.tsx
+++ src/components/Snake/Snake.tsx
@@ -1,27 +1,27 @@
-import React, { useRef } from 'react';
-import { Canvas } from '../Canvas/Canvas';
-import classes from './Snake.module.css';
-import {GameLogic} from './GameLogic';
-import {Draw} from '../Draw/Draw';
+import React, { useRef } from "react";
+import { Canvas } from "../Canvas/Canvas";
+import classes from "./Snake.module.css";
+import { GameLogic } from "./GameLogic";
+import { Draw } from "../Draw/Draw";
 
-interface GameProps{}
+interface GameProps {}
 
 export const Snake: React.FC<GameProps> = ({}) => {
-
   const canvaRef = useRef<HTMLCanvasElement>(null);
-  const {snakeBody} = GameLogic();
+  const { snakeBody, onKeyDownHandler } = GameLogic();
   const drawGame = (ctx: CanvasRenderingContext2D) => {
-Draw({ctx, snakeBody})
-  }
+    Draw({ ctx, snakeBody });
+  };
   return (
     <div>
-    <h1>Snake it</h1>
-    <div className={classes ['Snake']}>
-   
-    <Canvas
-    ref={canvaRef}
-    draw={drawGame}/>
-    </div>
+      <h1>Snake it</h1>
+      <div
+        className={classes["Snake"]}
+        onKeyDown={onKeyDownHandler}
+        tabIndex={0}
+      >
+        <Canvas ref={canvaRef} draw={drawGame} />
+      </div>
     </div>
   );
-}
+};
