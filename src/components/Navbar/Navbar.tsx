838dd23cf4708bbb6f1b3ffa7fb962ec2af52cce
olga-kacala <olga.kacala@gmail.com>
2023-04-17 19:51:22 +0200

    Deploy

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import classes from "./Navbar.module.css" ;
 
-
 export const Navbar = (): JSX.Element => {
 
   const navigate = useNavigate();
@@ -9,7 +8,7 @@ export const Navbar = (): JSX.Element => {
     <div className={classes ["Nav"]}>
       <img className={classes ["NavPic"]}
       alt="Bill Murray" 
-      src={"/img/billMurray.png"}
+      src={"../img/billMurray.png"}
       onClick={()=> {
         navigate('/')
       }}/>
